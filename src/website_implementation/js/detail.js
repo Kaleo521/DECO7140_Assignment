ba8a0dd51b74a01aa5e460a7812053655bb013b6
Kaleo521 <140377042+Kaleo521@users.noreply.github.com>
2023-10-15 15:33:28 +1000

    Update detail.js

--- src/website_implementation/js/detail.js
+++ src/website_implementation/js/detail.js
@@ -1,26 +1,13 @@
+; (function () {
+    // 获取所有小图片
+    let aImgSmall = document.querySelectorAll('.goods-imgs__small img');
+    // 获取大图片
+    let oImgBig = document.querySelector('.goods-imgs__big img');
 
-        // dom 操作 获取 菜单节点
-        let oDivMenu = document.querySelector('.menu');
-        let oDivNav = document.querySelector('.nav');
-        let oDivHeader = document.querySelector('.header');
-        oDivMenu.onclick = function (e) {
-            e.stopPropagation();
-            oDivNav.classList.toggle('show');
-
-            oDivHeader.onclick = function () {
-                oDivNav.classList.remove('show');
-            }
+    for (var i = 0; i < aImgSmall.length; i++) {
+        // 给小图片添加点击事件， 点击小图片切换大图片的链接
+        aImgSmall[i].onclick = function () {
+            oImgBig.src = this.src;
         }
-        // 获取所有小图片
-        let aImgSmall = document.querySelectorAll('.goods-imgs__small img');
-        // 获取大图片
-        let oImgBig = document.querySelector('.goods-imgs__big img');
-
-        for (var i = 0; i < aImgSmall.length; i++) {
-            // 给小图片添加点击事件， 点击小图片切换大图片的链接
-            aImgSmall[i].onclick = function () {
-                oImgBig.src = this.src;
-            }
-        }
-
-
+    }
+})()
