e542af50545449de0a1216cf547365df10cfa3ea
Kaleo521 <140377042+Kaleo521@users.noreply.github.com>
2023-10-21 18:32:34 +1000

    Update detail.js

--- src/website_implementation/js/detail.js
+++ src/website_implementation/js/detail.js
@@ -1,13 +1,25 @@
 ; (function () {
-    // 获取所有小图片
+    // Get all the small pictures
     let aImgSmall = document.querySelectorAll('.goods-imgs__small img');
-    // 获取大图片
+    // Get a big picture
     let oImgBig = document.querySelector('.goods-imgs__big img');
 
     for (var i = 0; i < aImgSmall.length; i++) {
-        // 给小图片添加点击事件， 点击小图片切换大图片的链接
+        // Add a click event to the small picture, and click the small picture to switch the link of the big picture.
         aImgSmall[i].onclick = function () {
             oImgBig.src = this.src;
         }
     }
-})()
+
+    let oImgAvatar = document.querySelector('.goods-info__avatar img');
+    let w = oImgAvatar.clientWidth;
+    let h = oImgAvatar.clientHeight;
+    console.log(w, h);
+    if (w > h) {
+        oImgAvatar.style.width = 'auto';
+        oImgAvatar.style.height = '100%';
+    } else {
+        oImgAvatar.style.width = '100%';
+        oImgAvatar.style.height = 'auto';
+    }
+})();
