11791e317361d30e16b366e7d0c1cbca1422d4d2
Kaleo521 <140377042+Kaleo521@users.noreply.github.com>
2023-10-22 15:41:36 +1000

    Update detail.js

--- src/website_implementation/js/detail.js
+++ src/website_implementation/js/detail.js
@@ -1,25 +1,40 @@
-; (function () {
-    // Get all the small pictures
+// Immediately Invoked Function Expression (IIFE) to encapsulate the code and prevent possible interference with other scripts
+(function () {
+    
+    // Select all images inside the '.goods-imgs__small' class
     let aImgSmall = document.querySelectorAll('.goods-imgs__small img');
-    // Get a big picture
+    
+    // Select the big image inside the '.goods-imgs__big' class
     let oImgBig = document.querySelector('.goods-imgs__big img');
 
+    // Loop through each of the small images
     for (var i = 0; i < aImgSmall.length; i++) {
-        // Add a click event to the small picture, and click the small picture to switch the link of the big picture.
+        // Attach a click event listener to each small image
         aImgSmall[i].onclick = function () {
+            // When a small image is clicked, set the source of the big image to the source of the clicked small image
             oImgBig.src = this.src;
         }
     }
 
+    // Select the avatar image inside the '.goods-info__avatar' class
     let oImgAvatar = document.querySelector('.goods-info__avatar img');
+    
+    // Get the width and height of the avatar image
     let w = oImgAvatar.clientWidth;
     let h = oImgAvatar.clientHeight;
+    
+    // Log the width and height of the avatar image to the console
     console.log(w, h);
+
+    // Check the dimensions of the avatar image
     if (w > h) {
+        // If the image width is greater than its height, adjust its width automatically and set its height to fill the container
         oImgAvatar.style.width = 'auto';
         oImgAvatar.style.height = '100%';
     } else {
+        // If the image height is greater or equal to its width, adjust its height automatically and set its width to fill the container
         oImgAvatar.style.width = '100%';
         oImgAvatar.style.height = 'auto';
     }
+
 })();
