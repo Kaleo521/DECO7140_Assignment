fe4301bd6aab5a7d68055c6efd91fd08b94c2f24
Kaleo521 <140377042+Kaleo521@users.noreply.github.com>
2023-10-22 15:41:39 +1000

    Update index.js

--- src/website_implementation/js/index.js
+++ src/website_implementation/js/index.js
@@ -1,28 +1,38 @@
+// Select all images inside the '.artists' class
 let aImgArtist = document.querySelectorAll('.artists img');
+
+// Loop through each of the artist images
 for (let i = 0; i < aImgArtist.length; i++) {
+
+    // Get the width and height of the current artist image
     let w = aImgArtist[i].clientWidth;
     let h = aImgArtist[i].clientHeight;
+
+    // Check the dimensions of the current artist image
     if (w > h) {
+        // If the image width is greater than its height, adjust its width automatically and set its height to fill the container
         aImgArtist[i].style.width = 'auto';
         aImgArtist[i].style.height = '100%';
     } else {
+        // If the image height is greater or equal to its width, adjust its height automatically and set its width to fill the container
         aImgArtist[i].style.width = '100%';
         aImgArtist[i].style.height = 'auto';
     }
 }
 
+// Initialize the Swiper slider for the '.banner .swiper' class
 var mySwiper = new Swiper('.banner .swiper', {
-    autoplay: true,
-    loop: true, // Loop mode options
+    autoplay: true,         // Enable autoplay for the slider
+    loop: true,             // Enable loop mode, so the slider restarts after the last slide
 
-    // If you need a pager
+    // Configuration for the pager dots at the bottom of the slider
     pagination: {
         el: '.swiper-pagination',
     },
 
-    // If you need the forward and backward button
+    // Configuration for the forward and backward navigation buttons
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
-})
+});
