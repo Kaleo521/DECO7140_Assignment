6bb075a8ee64c6bd45b727a904109b4e82dff0b7
Kaleo521 <140377042+Kaleo521@users.noreply.github.com>
2023-10-22 15:41:41 +1000

    Update menu.js

--- src/website_implementation/js/menu.js
+++ src/website_implementation/js/menu.js
@@ -1,13 +1,30 @@
+// Select the '.menu' element from the document
+let oDivMenu = document.querySelector('.menu');
 
-    let oDivMenu = document.querySelector('.menu');
-    let oDivNav = document.querySelector('.nav');
-    let oDivHeader = document.querySelector('.header');
-    oDivMenu.onclick = function (e) {
-        console.log(e);
-        e.stopPropagation();
-        oDivNav.classList.toggle('show');
-        console.log(oDivNav.classList);
-        oDivHeader.onclick = function () {
-            oDivNav.classList.remove('show');
-        }
+// Select the '.nav' element from the document
+let oDivNav = document.querySelector('.nav');
+
+// Select the '.header' element from the document
+let oDivHeader = document.querySelector('.header');
+
+// Attach a click event listener to the 'menu' div
+oDivMenu.onclick = function (e) {
+
+    // Log the event object to the console
+    console.log(e);
+    
+    // Prevent the event from propagating up to parent elements
+    e.stopPropagation();
+
+    // Toggle the 'show' class on the 'nav' div, effectively showing or hiding it
+    oDivNav.classList.toggle('show');
+
+    // Log the list of classes on the 'nav' div to the console
+    console.log(oDivNav.classList);
+    
+    // Attach a click event listener to the 'header' div
+    oDivHeader.onclick = function () {
+        // Remove the 'show' class from the 'nav' div, hiding it
+        oDivNav.classList.remove('show');
     }
+}
